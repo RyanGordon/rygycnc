9f9110c3c7bd38a4def64dd4cbd00f5230ed4bd2
Ryan Gordon <ryan@quizlet.com>
2014-05-01 20:04:43 -0700

    Working on some disconnect/connection improvements

--- chrome/js/usb.js
+++ chrome/js/usb.js
@@ -1,11 +1,39 @@
 var VENDOR_ID = 0x03EB; // integer = 1003
 var PRODUCT_ID = 0x214F; // integer = 8527
 
-window.onload = function() {
-	document.getElementById('openConnection').addEventListener('click', requestPermissions);
+var HIDConnectionId = null;
+var HIDReady = false;
+
+function findHIDDevice() {
+	chrome.hid.getDevices({"vendorId": VENDOR_ID, "productId": PRODUCT_ID}, function(devices) {
+		connectToHIDDevice(devices[0].deviceId);
+	});
 }
 
-var requestPermissions = (function() {
+function connectToHIDDevice(deviceId) {
+	chrome.hid.connect(deviceId, function(connection) {
+		HIDConnectionId = connection.connectionId;
+		HIDReady = true;
+		console.log("Device connected with connection id "+HIDConnectionId);
+		document.getElementById('connect').style.display = "none";
+		document.getElementById('connect').text = 'Connect';
+
+		document.getElementById('disconnect').style.display = "block";
+	});
+}
+
+window.onload = function() {
+	document.getElementById('connect').addEventListener('click', connectDevice);
+	document.getElementById('disconnect').addEventListener('click', disconnectDevice);
+}
+
+var connectDevice = (function() {
+	if (HIDConnectionId != null) {
+		console.log("Already connected to device with connection id "+HIDConnectionId+". Disconnect first.");
+		return 0;
+	}
+
+	document.getElementById('connect').text = 'Connecting';
 	chrome.permissions.request({
 		'permissions': [{
 			'usbDevices': [{
@@ -18,13 +46,34 @@ var requestPermissions = (function() {
 
 var permissionsCallback = (function(result) {
 	if (result) {
-		document.getElementById('openConnection').text = 'Connected';
-		document.getElementById('openConnection').disabled = true;
 		console.log('App was granted the "usbDevices" permission.');
-		chrome.hid.getDevices({"vendorId": VENDOR_ID, "productId": PRODUCT_ID}, logCallback);
+		findHIDDevice();
 	} else {
+		document.getElementById('connect').text = 'Connect';
 		console.log('App was NOT granted the "usbDevices" permission.');
 	}
+});
+
+var disconnectDevice = (function() {
+	document.getElementById('disconnect').style.display = "none";
+	document.getElementById('disconnect').text = 'Disconnecting';
+	if (HIDConnectionId != null) {
+		chrome.hid.disconnect(HIDConnectionId, disconnectedCallback);
+	} else {
+		disconnectedCallback();
+	}
+});
+
+var disconnectedCallback = (function() {
+	console.log("Device with connection id "+HIDConnectionId+" is disconnected.");
+	HIDConnectionId = null;
+	HIDReady = false;
+
+	document.getElementById('disconnect').style.display = "none";
+	document.getElementById('disconnect').text = 'Disconnect';
+
+	document.getElementById('connect').style.display = "block";
+	document.getElementById('connect').text = 'Connect';
 });
 
 var logCallback = (function(data) {
