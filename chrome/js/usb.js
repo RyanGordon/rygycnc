dd2fdf868a4042ce0ca3a7d052e076c8c8cf039d
Ryan Gordon <ryan@quizlet.com>
2014-05-02 15:00:47 -0700

    Putting in some updates and test button

--- chrome/js/usb.js
+++ chrome/js/usb.js
@@ -4,9 +4,60 @@ var PRODUCT_ID = 0x214F; // integer = 8527
 var HIDConnectionId = null;
 var HIDReady = false;
 
+chrome.runtime.onSuspend.addListener(disconnectDevice);
+
+window.onload = function() {
+	document.getElementById('connect').addEventListener('click', connectDevice);
+	document.getElementById('disconnect').addEventListener('click', disconnectDevice);
+	document.getElementById('send_test_data').addEventListener('click', sendTestDataToDevice);
+}
+
+var sendTestDataToDevice = (function() {
+	if (HIDReady !== true) {
+		console.log("Device not connected. Please connect first.");
+		return 0;
+	}
+
+	console.log("Sending packet to device.");
+	var data = [0x81, 0x81, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02, 0x02];
+	sendData(data, function() {
+		console.log("Packet has been sent.");
+	});
+	
+});
+
+function sendData(data, callback) {
+	var dataBuffer = new ArrayBuffer(64);
+	var dataArray = new Uint8Array(dataBuffer);
+	var dataLength = data.length;
+	for (var i=0; i< dataLength; i++) {
+		dataArray[i] = data[i];
+	}
+	console.log(data);
+	console.log(dataArray);
+	console.log(readFromArrayBuffer(dataBuffer));
+
+	chrome.hid.send(HIDConnectionId, 0, dataBuffer, callback);
+}
+
+function readFromArrayBuffer(arrayBuffer) {
+	var dataArrayTest = new Uint8Array(arrayBuffer);
+	console.log(dataArrayTest);
+}
+
+function recieveData(size, dataCallback) {
+	chrome.hid.recieve(HIDConnectionId, size, dataCallback);
+}
+
 function findHIDDevice() {
 	chrome.hid.getDevices({"vendorId": VENDOR_ID, "productId": PRODUCT_ID}, function(devices) {
-		connectToHIDDevice(devices[0].deviceId);
+		console.log(devices);
+		if (devices.size === 0) {
+			console.log("No connected RYGY CNC devices were found");
+			document.getElementById('connect').text = 'Connect';
+		} else {
+			connectToHIDDevice(devices[0].deviceId);
+		}
 	});
 }
 
@@ -22,13 +73,8 @@ function connectToHIDDevice(deviceId) {
 	});
 }
 
-window.onload = function() {
-	document.getElementById('connect').addEventListener('click', connectDevice);
-	document.getElementById('disconnect').addEventListener('click', disconnectDevice);
-}
-
 var connectDevice = (function() {
-	if (HIDConnectionId != null) {
+	if (HIDConnectionId !== null) {
 		console.log("Already connected to device with connection id "+HIDConnectionId+". Disconnect first.");
 		return 0;
 	}
@@ -55,9 +101,8 @@ var permissionsCallback = (function(result) {
 });
 
 var disconnectDevice = (function() {
-	document.getElementById('disconnect').style.display = "none";
 	document.getElementById('disconnect').text = 'Disconnecting';
-	if (HIDConnectionId != null) {
+	if (HIDConnectionId !== null) {
 		chrome.hid.disconnect(HIDConnectionId, disconnectedCallback);
 	} else {
 		disconnectedCallback();
