844a7bdd882e0a9a37a110455e6da57232b5e1ab
Ryan Gordon <ryan@quizlet.com>
2014-05-14 00:10:43 -0700

    Adding ability for scene to resize properly

--- chrome/background.js
+++ chrome/background.js
@@ -3,6 +3,8 @@ chrome.app.runtime.onLaunched.addListener(openMainWindow);
 var openMainWindow = (function() {
 	chrome.app.window.create('window.html', {
 		'state': 'normal',
+		minWidth: 600,
+		minHeight: 400,
 		bounds: {
 			width: 800,
 			height: 500
--- chrome/js/gcode_scene.js
+++ chrome/js/gcode_scene.js
@@ -5,13 +5,45 @@ $(document).on('ready', function() {
 
 var scene, renderer, camera;
 
-var INITIAL_WIDTH = 400;
-var INITIAL_HEIGHT = 340;
+var SCENE_WIDTH = 500;
+var SCENE_HEIGHT = 400;
+
+var setSceneSize = (function() {
+  var newwidth = Math.round($(window).width()*0.50);
+  var heightratio;
+  if ($(window).height() <= 450) {
+    heightratio = 0.65;
+  } else if ($(window).height() <= 500) {
+    heightratio = 0.675;
+  } else if ($(window).height() <= 550) {
+    heightratio = 0.68;
+  } else if ($(window).height() > 810) {
+    heightratio = 0.73;
+  } else if ($(window).height() > 700) {
+    heightratio = 0.72;
+  } else if ($(window).height() > 650) {
+    heightratio = 0.71;
+  } else if ($(window).height() > 600) {
+    heightratio = 0.695;
+  } else {
+    heightratio = 0.70;
+  }
+  var newheight = Math.round($(window).height()*heightratio);
+
+  if (newheight !== SCENE_HEIGHT || newwidth !== SCENE_WIDTH) {
+    SCENE_HEIGHT = newheight;
+    SCENE_WIDTH = newwidth;
+    renderer.setSize(SCENE_WIDTH, SCENE_HEIGHT);
+    camera.aspect = SCENE_WIDTH / SCENE_HEIGHT;
+    camera.updateProjectionMatrix();
+    console.log($(window).width(), newwidth, $(window).height(), heightratio, newheight);
+  }
+});
 
 function createScene() {
 
   // set some camera attributes
-  var VIEW_ANGLE = 45, ASPECT = INITIAL_WIDTH / INITIAL_HEIGHT, NEAR = 0.1, FAR = 10000;
+  var VIEW_ANGLE = 45, ASPECT = SCENE_WIDTH / SCENE_HEIGHT, NEAR = 0.1, FAR = 10000;
 
   // create a WebGL renderer, camera and a scene
   renderer = new THREE.WebGLRenderer();
@@ -24,13 +56,15 @@ function createScene() {
   camera.position.z = 300;
 
   // start the renderer
-  renderer.setSize(INITIAL_WIDTH, INITIAL_HEIGHT);
+  setSceneSize();
 
   // attach the render-supplied DOM element
   return renderer.domElement;
 }
 
 function renderScene() {
+
+  $(window).resize(setSceneSize);
 
   // create a point light
   var pointLight = new THREE.PointLight(0xFFFFFF);
@@ -57,5 +91,11 @@ function renderScene() {
   // add the sphere to the scene
   scene.add(sphere);
 
+  renderer.render(scene, camera);
+  //render();
+}
+
+function render() {
+  requestAnimationFrame(render);
   renderer.render(scene, camera);
 }
