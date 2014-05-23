17d9d20971fd5e6d6d10b31e16f4e3662b85a99e
Ryan Gordon <ryan@quizlet.com>
2014-05-22 20:51:04 -0700

    Adding ability to move camera in scene and adding axises

--- chrome/background.js
+++ chrome/background.js
@@ -3,10 +3,10 @@ chrome.app.runtime.onLaunched.addListener(openMainWindow);
 var openMainWindow = (function() {
 	chrome.app.window.create('window.html', {
 		'state': 'normal',
-		minWidth: 600,
+		minWidth: 700,
 		minHeight: 400,
 		bounds: {
-			width: 800,
+			width: 1000,
 			height: 500
 		}
 	});
